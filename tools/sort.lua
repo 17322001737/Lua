c7d0125edff3427b5094b8f5fd8b59c2680a40b6
GoHo <40389925+17322001737@users.noreply.github.com>
2020-01-06 11:48:16 +0800

    代码优化2

--- tools/sort.lua
+++ tools/sort.lua
@@ -2,21 +2,20 @@ SORT_TYPE = {
 	Upper = 1,
 	Lower = 2,
 }
-local tab = {{id = 4, can_get = 0, num = 10}, {id = 1, can_get = 0, num = 6}, {id = 3,  can_get = 1, num = 100}, {id = 5,  can_get = 1, num = 123}, {id = 2,  can_get = 0, num = 6}, {id = 6, can_get = 0, num = 2}}
-SortTools = {}
 
 -- tab 需要排序的表
 -- ... 参数的的优先级按照顺序降低
 -- SORT_TYPE.Upper 升序
 -- SORT_TYPE.Lower 降序
--- 示例 SortMix(tab, { {SORT_TYPE.Upper, "can_get"}, {SORT_TYPE.Lower, "id"} })
+-- 示例 SortMix(tab, {SORT_TYPE.Upper, "can_get"}, {SORT_TYPE.Lower, "id"})
+SortTools = {}
 function SortTools.Sort(tab, ...)
-    local params = (...)
+    local params = {...}
     local sort_fun = function (a, b)
         local left = 1000
         local right = 1000
         local sort_type, sort_key
-        for k, v in pairs(params) do
+        for k, v in ipairs(params) do
             sort_type = v[1]
             sort_key = v[2]
             if sort_type == SORT_TYPE.Upper then
@@ -40,11 +39,9 @@ function SortTools.Sort(tab, ...)
     table.sort(tab, sort_fun)
 end
 
-SortTools.Sort(tab, {
-	{SORT_TYPE.Lower, "can_get"},
-	{SORT_TYPE.Lower, "id"}
-})
+local tab = {{id = 4, can_get = 0, num = 10}, {id = 1, can_get = 0, num = 6}, {id = 3,  can_get = 1, num = 100}, {id = 5,  can_get = 1, num = 123}, {id = 2,  can_get = 0, num = 6}, {id = 6, can_get = 0, num = 2}}
+SortTools.Sort(tab, {SORT_TYPE.Lower, "can_get"}, {SORT_TYPE.Lower, "id"})
 
-for _, v in pairs(tab) do
+for _, v in ipairs(tab) do
 	print(v.id .. " ---- " .. v.can_get)
 end
